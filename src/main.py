0e9460c91b8a48273422a2dfd2f414f74705b056
Rakibul Haque <r.haque@tu-braunschweig.de>
2021-05-24 19:52:10 +0200

    main.py and requirements.txt created

---
+++ src/main.py
@@ -0,0 +1,20 @@
+from google.cloud import storage
+from google.cloud import bigquery
+
+
+def yourEntrypoint(event, context):
+    """
+    Trigger function for individual processing of a file
+    :param event: Contains all meta information like bucket name, filename etc.
+    :param context: Context-Object contains context.event_id, context.timestamp, context.event_type, context.resource
+    :return: None
+    """
+    # Client-Initialization
+    storageClient = storage.Client()
+    bigqueryClient = bigquery.Client()
+
+    # Rename for Readability
+    file = event
+
+    # Path to file in Bucket
+    uriFilePath = "gs://{}/{}".format(file["bucket"], file["name"])
